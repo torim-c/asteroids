94d59bc50711d5c3d80632c01affad8474746dc9
torim-c <mocanu.v.cezar@gmail.com>
2025-10-25 03:26:22 +0300

    added groupings for Player(), redone drawing and update methods

--- main.py
+++ main.py
@@ -7,13 +7,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-
+    #initial config
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #do not move under game loop else flicker happens
     clock = pygame.time.Clock()                 #FPS control
     dt = 0
 
+
+    #groups
+    updatable = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)            #always call this BEFORE instancing player. why? because then the Player() instance is not added to the groups! thus no draw or update!
+
+
+    #player position
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #the two args affect player initial position
+
+
 
     #infinite loop for running
     while True:
@@ -21,15 +31,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-
+        
         #all of these will be updated while the game runs
-        player.update(dt) #update player movement
-
-              
-        screen.fill((0, 0, 0))                  #black fill of screen #background color
+        updatable.update(dt)                    # player.update(dt) movement legacy BEFORE GROUPING
 
         
-        player.draw(screen)                     #draw player after screen fill otherwise you can't see him xD
+        screen.fill((0, 0, 0))                  #black fill of screen #background color
+
+        for drawed in drawable:                 #draw player after screen fill otherwise you can't see him xD #player.draw(screen) legacy BEFORE GROUPING
+            drawed.draw(screen)
+        
+                     
 
         pygame.display.flip()                   #update screen
         dt = clock.tick(60)/1000                #retuns the amount of time that has passed since last time it was called - DELTA TIME
